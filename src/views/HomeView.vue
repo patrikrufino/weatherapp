94d8c5895a20661e9a47e41f2d5ee77ea6eb5e6c
Patrik Rufino <patrik.rufino@gmail.com>
2023-09-11 07:41:54 -0300

    feat: Create redirect Route

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,6 +24,7 @@
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
             class="py-2 cursor-pointer hover:bg-weather-primary px-4 rounded-md w-full border-b border-[#96969620]"
+            @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -36,6 +37,21 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const mapboxAPIKey =
   "pk.eyJ1IjoicGF0cmlrcnVmaW5vIiwiYSI6ImNsbGxtNHVzeDFkNjAzZWx3Y2ljZDUxdXQifQ.29iMTzLTJfbFrVzRb8zaBA";
